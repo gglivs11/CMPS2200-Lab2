0825e346bab0b0d24f4d4c1065f3f2674a2d8b2f
Grace Livaudais <123023175+gglivs11@users.noreply.github.com>
2023-02-09 14:43:55 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,103 @@
+"""
+CMPS 2200  Recitation 2
+"""
+
+### the only imports needed are here
+import tabulate
+import time
+###
+
+def simple_work_calc(n, a, b):
+	"""Compute the value of the recurrence $W(n) = aW(n/b) + n
+
+	Params:
+	n......input integer
+	a......branching factor of recursion tree
+	b......input split factor
+
+	Returns: the value of W(n).
+	"""
+	# TODO
+	pass
+
+def test_simple_work():
+	""" done. """
+	assert work_calc(10, 2, 2) == #TODO
+	assert work_calc(20, 3, 2) == #TODO
+	assert work_calc(30, 4, 2) == #TODO
+
+def work_calc(n, a, b, f):
+	"""Compute the value of the recurrence $W(n) = aW(n/b) + f(n)
+
+	Params:
+	n......input integer
+	a......branching factor of recursion tree
+	b......input split factor
+	f......a function that takes an integer and returns 
+           the work done at each node 
+
+	Returns: the value of W(n).
+	"""
+	# TODO
+	pass
+
+def span_calc(n, a, b, f):
+	"""Compute the span associated with the recurrence $W(n) = aW(n/b) + f(n)
+
+	Params:
+	n......input integer
+	a......branching factor of recursion tree
+	b......input split factor
+	f......a function that takes an integer and returns 
+           the work done at each node 
+
+	Returns: the value of W(n).
+	"""
+	# TODO
+	pass
+
+def test_work():
+	""" done. """
+	assert work_calc(10, 2, 2,lambda n: 1) == #TODO
+	assert work_calc(20, 1, 2, lambda n: n*n) == #TODO
+	assert work_calc(30, 3, 2, lambda n: n) == #TODO
+
+def compare_work(work_fn1, work_fn2, sizes=[10, 20, 50, 100, 1000, 5000, 10000]):
+	"""
+	Compare the values of different recurrences for 
+	given input sizes.
+
+	Returns:
+	A list of tuples of the form
+	(n, work_fn1(n), work_fn2(n), ...)
+	
+	"""
+	result = []
+	for n in input_sizes:
+		# compute W(n) using current a, b, f
+		result.append((
+			n,
+			work_fn1(n),
+			work_fn2(n)
+			))
+	return result
+
+def print_results(results):
+	""" done """
+	print(tabulate.tabulate(results,
+							headers=['n', 'W_1', 'W_2'],
+							floatfmt=".3f",
+							tablefmt="github"))
+
+def test_compare_work():
+	# curry work_calc to create multiple work
+	# functions taht can be passed to compare_work
+    
+	# create work_fn1
+	# create work_fn2
+
+    res = compare_work(work_fn1, work_fn2)
+	print(res)
+
+def test_compare_span():
+	# TODO
